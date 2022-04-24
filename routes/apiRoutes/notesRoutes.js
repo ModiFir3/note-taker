cb9e98f89046bbd0a488d1a6bca55027812a2323
PaulS <psoliz333@gmail.com>
2022-04-23 21:25:04 -0500

    created delete button

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const db = require('../../db/db.json');
 const uniqid = require('uniqid');
 const fs = require('fs')
+const path = require('path')
 
 router.get('/notes', (req, res) => {
     let results = db
@@ -9,39 +10,47 @@ router.get('/notes', (req, res) => {
 });
 
 router.post('/notes', (req, res) => {
-    const newNote = req.body;
+    let newNote = req.body;
     newNote.id = uniqid();
+    let dbJoin = path.join(__dirname, '../../db/db.json');
 
     if (!newNote) {
-        res.send(400)
+        res.send(400);
+        db = []
+        return;
     }
 
     db.push(newNote)
 
-    fs.writeFile('../../db/db.json', JSON.stringify(newNote) , (err) => {
+    fs.writeFile(dbJoin, JSON.stringify(newNote) , (err) => {
         if (err) {
             console.log (err)
         }
         console.log('success')
     })
+
+    return res.json(newNote)
 });
 
 router.delete('/notes/:id', (req, res) => {
-    const params = [req.params.id];
+    let params = req.params.id;
+    let dbJoin = path.join(__dirname, '../../db/db.json');
 
-    for (let i = 0; i < db.length ; i++) {
+    for (let i = 0; i < db.length; i++) {
         if (db[i].id === params) {
             db.splice(i, 1);
+            break;
         }
     }
 
-    fs.writeFile('../../db/db.json', newNotes, (err) => {
+    fs.writeFile(dbJoin, JSON.stringify(db), (err) => {
         if (err) {
             console.log (err)
         }
         console.log('deleted')
-    })
-})
+    });
+    return res.json(db);
+});
 
 module.exports = router;
 
